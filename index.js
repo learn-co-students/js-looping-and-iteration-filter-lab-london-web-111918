56dff23c04fcd2dd5a936ab4fa1d8287814f09dc
Lingy94 <j.lingyx@gmail.com>
2019-01-07 09:38:04 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution in this file
+function findMatching(drivers, search) {
+  return drivers.filter(name => name.toLowerCase() === search.toLowerCase());
+};
+
+function fuzzyMatch(drivers, search) {
+  return drivers.filter(name => name.slice(0, search.length) === search);
+};
+
+function matchName(drivers, search) {
+  return drivers.filter(obj => obj["name"].toLowerCase() === search.toLowerCase())
+};
