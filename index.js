0ab77f711af56f417f80a5dadb47aa150e15bb14
NathanB09 <nathanbell09@gmail.com>
2019-01-07 09:55:19 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution in this file
+function findMatching(collection, item) {
+  const newArr = collection.filter(function (name) {
+    return name.toLowerCase() === item.toLowerCase();
+  }).slice();
+  return newArr;
+}
+
+function fuzzyMatch(collection, sub) {
+  const newArr = collection.filter(function (name) {
+    return name.substring(0, sub.length) === sub;
+  });
+  return newArr;
+}
+
+function matchName(collection, name) {
+  const newArr = collection.filter(function (obj) {
+    return obj.name === name;
+  });
+  return newArr;
+}
